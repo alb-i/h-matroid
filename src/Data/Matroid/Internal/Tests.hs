285bbe0a0cca762a29614058fa2130d4f6211e33
Immanuel Albrecht <mail@immanuel-albrecht.de>
2021-01-01 17:29:55 +0100

    added rk tests

---
+++ src/Data/Matroid/Internal/Tests.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+
+{-|
+Module      : Data.Matroid.Internal.Tests
+Description : 
+Copyright   : (c) Immanuel Albrecht, 2020-202x
+License     : BSD-3
+Maintainer  : mail@immanuel-albrecht.de
+Stability   : experimental
+Portability : POSIX
+
+This module provides internal helpers for the matroid package tests.
+
+-}
+
+module Data.Matroid.Internal.Tests where
+    
+import Data.List (foldl')
+import Data.Set (Set)
+import qualified Data.Set as S
+
+-- | Tests whether a given integer valued set function is indeed monotone increasing in at most unit steps
+is_monotone_unit_increasing :: Ord a =>       (Set a -> Int) {- ^ the rank function (or similar) -} 
+                                      -> [a] {- ^ sequence to check monotonicity with -} 
+                                      -> Bool
+is_monotone_unit_increasing rk e = result
+                           where  (_,result,_) = foldl' checkStep (S.empty :: Set a, True, 0 :: Int) e
+                                  checkStep (x0,False,r) _ = (x0,False,r) -- propagate error
+                                  checkStep (x0,True,r)  x = let x1 = S.insert x x0
+                                                                 r1 = rk x1
+                                                              in (x1, (r <= r1) && (r1 <= r + 1), r1)
--- test/Data/MatroidSpec.hs
+++ test/Data/MatroidSpec.hs
@@ -5,18 +5,18 @@ module Data.MatroidSpec (main, spec) where
 import Test.Hspec
 import Test.QuickCheck
 
-main :: IO ()
-main = hspec spec
+import Data.Matroid
+import Data.Matroid.Uniform
+import Data.Matroid.Internal.Tests
+
+import Data.Set (Set)
+import qualified Data.Set as S
+
 
 {-
 
 This is a todo list of things that could be tested:
 
-  -  rk is monotone increasing
-  -  rk(\emptyset) = 0 (important special case)
-  -  rk(X) <= |X| (subcardinal)
-  -  rk(X+{x}) <= rk(X) + 1  (unit increasing)
-  -  rk(A/\B) + rk(A\/B) <= rk(A) + rk(B) (submodular)
   
   -  indep X  &&  Y\subseteq X => indep Y (hereditary)
   -  indep \emptyset
@@ -33,8 +33,66 @@ This is a todo list of things that could be tested:
 
 -}
 
+
+-- | a generator for uniform matroids of a reasonable size
+gen_uniform_matroids :: Gen (UniformMatroid Int)
+gen_uniform_matroids = do r <- (arbitrary :: Gen Int) `suchThat` (>= 0) `suchThat` (<= 25) 
+                          n <- (arbitrary :: Gen Int) `suchThat` (>= r) `suchThat` (<= 100)
+                          return $ uniform n r
+                        
+  
+                          
+{- | test suite for rank axioms
+ 
+ The following properties are verified:
+
+  -  rk is monotone increasing
+  -  rk(X+{x}) <= rk(X) + 1  (unit increasing)
+  
+  -  rk(\emptyset) = 0 (important special case)
+  -  rk(X) <= |X| (subcardinal)
+
+  -  rk(A/\B) + rk(A\/B) <= rk(A) + rk(B) (submodular)
+ 
+-}
+rk_properties_suite :: Matroid m a => Gen (m a) {- ^ matroid test case generator -} -> SpecWith ()
+rk_properties_suite genMatroids = context "rk properties" $ do
+   it "rk is monotone increasing" $ property $ do
+    m <- genMatroids
+    e <- shuffle $ S.toList $ groundset m
+    return $ is_monotone_unit_increasing (rk m) e
+   it "rk({}) == 0" $ property $ do
+     m <- genMatroids
+     return $ rk m S.empty == 0
+   it "rk is subcardinal" $ property $ do
+      m <- genMatroids
+      x <- sublistOf $ S.toList $ groundset m
+      return $ (rk m) (S.fromList x) <= length x
+   it "rk is submodular" $ property $ do
+      m <- genMatroids
+      a0 <- sublistOf $ S.toList $ groundset m
+      b0 <- sublistOf $ S.toList $ groundset m
+      let a = S.fromList a0
+          b = S.fromList b0
+        in return $ (rk m (a `S.union` b)) + (rk m (a `S.intersection` b)) <= (rk m a) + (rk m b)
+  
+main :: IO ()
+main = hspec spec
+
+
 spec :: Spec
 spec = do
-  describe "someFunction" $ do
-    it "should work fine" $ do
-      True `shouldBe` False
+    describe "Data.Matroid.Uniform.uniform" $ do
+      rk_properties_suite gen_uniform_matroids
+
+      
+      
+
+{-
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "Data.Matroid.Uniform.uniform" $ do
+  it "test" $ True `shouldBe` True
+  -}
