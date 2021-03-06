4ad242b7fba582cdd281d30ad4eb0bcb177b6314
Immanuel Albrecht <mail@immanuel-albrecht.de>
2021-03-06 17:23:32 +0100

    added tests of field properties for Z/Zp

--- test/Data/FieldsSpec.hs
+++ test/Data/FieldsSpec.hs
@@ -2,6 +2,7 @@
 
 module Data.FieldsSpec (main, spec) where
 
+import Data.Fields.Zp
 import Test.Hspec
 import Test.QuickCheck
 
@@ -14,4 +15,12 @@ main = hspec spec
 -- | all the tests
 spec :: Spec
 spec = do
+    describe "Z/Z141650939" $ do fieldPropertiesSuite ((do x <- chooseInt(0,141650938)
+                                                           return $ prjModP 141650939 x) :: Gen XModP)
+    describe "Z/Z4001" $ do fieldPropertiesSuite ((do x <- chooseInt(0,4000)
+                                                      return $ prjModP 4001 x) :: Gen XModP)
+    describe "Z/Z4003" $ do fieldPropertiesSuite ((do x <- chooseInt(0,4002)
+                                                      return $ prjModP 4003 x) :: Gen XModP)
+    describe "Z/Z13" $ do fieldPropertiesSuite ((do x <- chooseInt(0,4002)
+                                                    return $ prjModP 13 x) :: Gen XModP)
     describe "Rational" $ do fieldPropertiesSuite (arbitrary :: Gen Rational)
